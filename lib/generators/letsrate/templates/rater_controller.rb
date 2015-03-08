88b84a80bf0d9afb1a461fce182b4c9ab70a1a80
Fari Liang <fariliang@users.noreply.github.com>
2015-03-08 16:09:29 -0400

    Update rater_controller.rb

--- lib/generators/letsrate/templates/rater_controller.rb
+++ lib/generators/letsrate/templates/rater_controller.rb
@@ -1,14 +1,13 @@
 class RaterController < ApplicationController
 
-  def create
-    if user_signed_in?
-      obj = params[:klass].classify.constantize.find(params[:id])
-      obj.rate params[:score].to_i, current_user, params[:dimension]
-
-      render :json => true
-    else
-      render :json => false
-    end
+  def create                                  
+    if current_user.present?
+      obj = eval "#{params[:klass]}.find(#{params[:id]})"     
+      if params[:dimension].present?
+        obj.rate params[:score].to_i, current_user.id, "#{params[:dimension]}"       
+      else
+        obj.rate params[:score].to_i, current_user.id 
+      end
   end
-
+  
 end
